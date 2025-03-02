6bdcc4600265550114c96cf3e30b8656eaa28b10
pwups <aylapmacc@gmail.com>
2025-03-02 14:58:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const
-  { Client } = require('discord.js-selfbot-v13'),
-  client = new Client({checkUpdate: false,}),
-  reloadPresence = require("./config.js"),
-  keepAlive = require('./keep_alive.js');
+const { Client, GatewayIntentBits } = require('discord.js-selfbot-v13');
+
+const client = new Client({
+    checkUpdate: false,
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+const reloadPresence = require('./config.js');
+const keepAlive = require('./keepAlive.js');
 
 keepAlive();
 
 if (!process.env.TOKEN) {
-  console.error("Add a token inside Secrets.");
-  process.exit();
+    console.error("Add a token inside Secrets.");
+    process.exit();
 }
 
 client.login(process.env.TOKEN);
 
-  console.clear();
-  client.on("ready", async () => {
-        global.startTime = new Date();
-        reloadPresence(client);
-    })
+console.clear();
+client.on("ready", async () => {
+    global.startTime = new Date();
+    reloadPresence(client);
+});
